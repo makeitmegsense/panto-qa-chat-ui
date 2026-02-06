0204e5ff2a2ba46e30648154917f9031153f0374
Meghna Sen <meghnasen1001@gmail.com>
2026-02-06 17:00:41 +0530

    vercel changes for execute

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,12 @@
 import QAChatbot from "@/components/chat/QAChatbot";
+import ExecuteTestFlowPage from "src/components/chat/ExecuteTestFlowPage.tsx"
+
+
 
 const Index = () => {
   return (
     <div className="min-h-screen gradient-dark">
-      <QAChatbot />
+      <ExecuteTestFlowPage/>
     </div>
   );
 };
